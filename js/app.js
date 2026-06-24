221173edbff8cfcf80214f950f492d778665386b
Juliette1816-j <140426982+Juliette1816-j@users.noreply.github.com>
2026-06-24 16:44:36 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,3 +7,14 @@ document.getElementById("btnLogin").addEventListener("click", () => {
 
     alert("Botón funcionando correctamente");
 });
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabaseUrl = "https://jzuxaxlnguvsvlmymkge.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6dXhheGxuZ3V2c3ZsbXlta2dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyNTI2NjIsImV4cCI6MjA5NzgyODY2Mn0.DatIvM5O6mFz0qhR8tRreB0TCyB8pBMj5FBo0GmMEQo";
+
+const supabase = createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
+console.log("Conectado a Supabase");
