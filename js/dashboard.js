26c9d9517607de0965d25a2616baa4d60aa17f48
Juliette1816-j <140426982+Juliette1816-j@users.noreply.github.com>
2026-06-25 16:30:20 -0500

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabase = createClient(
     "https://jzuxaxlnguvsvlmymkge.supabase.co",
-    "TU_ANON_KEY"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6dXhheGxuZ3V2c3ZsbXlta2dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyNTI2NjIsImV4cCI6MjA5NzgyODY2Mn0.DatIvM5O6mFz0qhR8tRreB0TCyB8pBMj5FBo0GmMEQo"
 );
 
 const usuario = JSON.parse(localStorage.getItem("usuario"));
